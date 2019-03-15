b764eb465c1098c566c574126032ede87bdb8ee0
grace-mao23 <gmao00@stuy.edu>
2019-03-15 09:48:57 -0400

    Empty methods entered in

---
+++ Merge.java
@@ -0,0 +1,19 @@
+public class Merge {
+
+  public static void mergesort(int[]data) {
+
+  }
+
+  private static void mergesort(int[] data, int lo, int hi) {
+
+  }
+
+
+
+/*  mergesort(data,lo,hi):
+    if lo >= hi :
+      return
+    mergesort left side
+    mergesort right side
+    merge  */
+}
