09734c808f06fe6085166792bad81e42d0051489
grace-mao23 <gmao00@stuy.edu>
2019-03-15 09:52:36 -0400

    Filled in parts of pseudocode except final merging

--- Merge.java
+++ Merge.java
@@ -1,16 +1,25 @@
 public class Merge {
 
   public static void mergesort(int[]data) {
-
+    mergesort(data, 0, data.length-1);
   }
 
   private static void mergesort(int[] data, int lo, int hi) {
-
+    if (lo >= hi) { // most likely equal, when there is only one element
+      return;
+    }
+    int mid = (hi - lo) / 2;
+    // mergesort left
+    mergesort(data, lo, mid);
+    // mergesort right
+    mergesort(data, mid+1, hi);
+    // merge into data
   }
 
 
 
-/*  mergesort(data,lo,hi):
+/*  Pseudocode
+    mergesort(data,lo,hi):
     if lo >= hi :
       return
     mergesort left side
