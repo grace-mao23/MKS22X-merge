5e53c4159ee72c541f4781cb69e190515db303ee
grace-mao23 <gmao00@stuy.edu>
2019-03-15 10:13:23 -0400

    Merge sorted left and right by creating new arrays

--- Merge.java
+++ Merge.java
@@ -8,12 +8,21 @@ public class Merge {
     if (lo >= hi) { // most likely equal, when there is only one element
       return;
     }
-    int mid = (hi - lo) / 2;
-    // mergesort left
-    mergesort(data, lo, mid);
-    // mergesort right
-    mergesort(data, mid+1, hi);
-    // merge into data
+    int half = data.length / 2;
+    // mergesort left --> making left array
+    int[] left = new int[half];
+    for (int i = 0; i <= left.length; i++) {
+      left[i] = data[i];
+    }
+    mergesort(left, 0, left.length-1);
+    // mergesort right --> making right array
+    int[] right = new int[data.length - half];
+    for (int i = 0; i < right.length; i++) {
+      right[i] = data[i+half];
+    }
+    mergesort(right, 0, right.length-1);
+    // merge right and left into data
+    
   }
 
 
