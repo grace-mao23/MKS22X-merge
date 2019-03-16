d92bade74a478f352fa0b7c944ac4f66880f348c
grace-mao23 <gmao00@stuy.edu>
2019-03-16 10:17:39 -0400

    Used Mr. K's main method from quicksort to test runtime

--- Merge.java
+++ Merge.java
@@ -10,7 +10,7 @@ public class Merge {
     if (lo >= hi) { // most likely equal, when there is only one element
       return;
     }
-    System.out.println(Arrays.toString(data));
+  //  System.out.println(Arrays.toString(data));
     int half = data.length / 2;
     // mergesort left --> making left array
     int[] left = new int[half];
@@ -63,14 +63,51 @@ public class Merge {
         data[index] = right[right.length-1];
       }
     } */
-    System.out.println("M: "+Arrays.toString(data));
+    //System.out.println("M: "+Arrays.toString(data));
   }
 
-  public static void main(String[] args) {
+  /*public static void main(String[] args) {
     int[] t = new int[] { 2, 5, 3, 8, 1, 9, 0, 4, 7 };
     Merge.mergesort(t);
     System.out.println(Arrays.toString(t));
+  } */
+  public static void main(String[]args){
+    System.out.println("Size\t\tMax Value\tmerge/builtin ratio ");
+    int[]MAX_LIST = {1000000000,500,10};
+    for(int MAX : MAX_LIST){
+      for(int size = 31250; size < 2000001; size*=2){
+        long qtime=0;
+        long btime=0;
+        //average of 5 sorts.
+        for(int trial = 0 ; trial <=5; trial++){
+          int []data1 = new int[size];
+          int []data2 = new int[size];
+          for(int i = 0; i < data1.length; i++){
+            data1[i] = (int)(Math.random()*MAX);
+            data2[i] = data1[i];
+          }
+          long t1,t2;
+          t1 = System.currentTimeMillis();
+          Merge.mergesort(data2);
+          t2 = System.currentTimeMillis();
+          qtime += t2 - t1;
+          t1 = System.currentTimeMillis();
+          Arrays.sort(data1);
+          t2 = System.currentTimeMillis();
+          btime+= t2 - t1;
+          if(!Arrays.equals(data1,data2)){
+            System.out.println("FAIL TO SORT!");
+            System.exit(0);
+          }
+        }
+        System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
+      }
+      System.out.println();
+    }
   }
+
+
+
 
 
 /*  Pseudocode
